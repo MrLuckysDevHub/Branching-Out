210c4a3809bdd4c814a91b4b24d815702d4115bf
Mister Lucky <gluecksApplyJobs@gmail.com>
2025-10-22 17:46:27 +0200

    Added the feature filter by age

--- filter_users.py
+++ filter_users.py
@@ -1,19 +1,48 @@
 import json
 
-def filter_users_by_name(name):
+
+def get_user_data():
     with open("users.json", "r") as file:
         users = json.load(file)
-    
-    filtered_users = [user for user in users if user["name"].lower() == name.lower()]
-    
-    for user in filtered_users:
+    return users
+
+
+def filter_users_by_name(name):
+    users = get_user_data()
+    return [user for user in users if user["name"].lower() == name.lower()]
+
+
+def filter_users_by_age(age):
+    users = get_user_data()
+    return [user for user in users if user["age"] == age]
+
+
+def print_users(users):
+    for user in users:
         print(user)
 
+
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
-    
+    filter_option = (
+        input(
+            "What would you like to filter by? (Currently, only 'name' and 'age' is supported): "
+        )
+        .strip()
+        .lower()
+    )
+
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
-        filter_users_by_name(name_to_search)
+        print_users(filter_users_by_name(name_to_search))
+
+    elif filter_option == "age":
+        while True:
+            age_to_search = input("Enter a age to filter users: ").strip()
+            try:
+                age_to_search = int(age_to_search)
+                print_users(filter_users_by_age(age_to_search))
+                break
+            except ValueError:
+                print("Please enter a valid integer for age.")
     else:
         print("Filtering by that option is not yet supported.")
